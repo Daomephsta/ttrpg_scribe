adc2861c76c2f5f75fcc768eabcaa514e88ca4ae
Daomephsta <dtnilfisk@gmail.com>
2023-07-17 12:21:46 +1200

    Add some built-in creature templates

---
+++ bestiary/dnd_scribe/bestiary/creature/templates.py
@@ -0,0 +1,32 @@
+from typing import Callable, TypedDict, Unpack
+
+from dnd_scribe.bestiary.creature import Creature, ability
+from dnd_scribe.bestiary.creature.ability import Ability
+from dnd_scribe.bestiary.creature.armour import ArmourClass
+
+
+def adjust_scores(abilities: set[str], adjuster: Callable[[str, int], int]):
+    # Apply filter to adjuster
+    adjuster = lambda ability, value: (adjuster(ability, value)\
+        if ability in abilities else value)
+    def template(args: Creature.TemplateArgs):
+        args['statistics'] = tuple(adjuster(ability.abbreviation, value)
+            for ability, value in zip(Ability, args['statistics']))
+    return template
+
+def civilised(args: Creature.TemplateArgs):
+    args['name'] = f"'Civilised' {args['name']}"
+    adjust_scores({'int'}, lambda _, val: max(6, val))(args)
+
+Scores = TypedDict('Scores', {'str': int, 'dex': int, 'con': int, 'int': int, 'wis': int, 'cha': int}, total=False)
+def scores(**values: Unpack[Scores]):
+    return adjust_scores(set(values.keys()), values.get)
+
+def armour(base_ac: int, reason: str, dex_limit: int = 10):
+    def template(args: Creature.TemplateArgs):
+        ac = base_ac + min(dex_limit, ability.mod(args['statistics'][1]))
+        args['ac'] = [ArmourClass(ac, f'{ac} ({reason})')]
+    return template
+
+leather_armour = armour(11, 'leather')
+studded_leather_armour = armour(12, 'studded leather')
