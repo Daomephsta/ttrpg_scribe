ca1b6a66b27564c1876841a08c7c925ac4d9897f
Daomephsta <dtnilfisk@gmail.com>
2023-10-14 02:46:24 +1300

    Update test project

--- notes/test/pages/foo/foo.py
+++ notes/test/pages/foo/foo.py
@@ -1,5 +1,6 @@
 from dnd_scribe.bestiary.apis import DND5EAPI
+from dnd_scribe.bestiary.creature.templates import rename
 
 exports = {
-    'CHICKEN': DND5EAPI.creature('eagle').derive(name='Chicken')
+    'CHICKEN': DND5EAPI.creature('eagle').derive(rename('Chicken'))
 }
