1ca61aa465c1ff46842b95795d2278defe5d2bc2
Daomephsta <dtnilfisk@gmail.com>
2023-12-25 22:37:12 +1300

    Replace full creature list with per pack list for speed

--- pf2e_bestiary/dnd_scribe/pf2e_bestiary/flask/__init__.py
+++ pf2e_bestiary/dnd_scribe/pf2e_bestiary/flask/__init__.py
@@ -1,27 +1,36 @@
 import json
-from dnd_scribe.pf2e_bestiary import foundry
 
 from flask import Blueprint, Flask, render_template
 
-from dnd_scribe.pf2e_bestiary.creature import PF2Creature
 import dnd_scribe.core.flask
+from dnd_scribe.pf2e_bestiary import foundry
+from dnd_scribe.pf2e_bestiary.creature import PF2Creature
 
 blueprint = Blueprint('pf2e_bestiary', __name__,
     static_folder='static',
     template_folder='templates',
     url_prefix='/creatures')
 
+_EXCLUDED_PACKS = [
+    'bestiary-ability-glossary-srd',
+    'bestiary-effects',
+    'bestiary-family-ability-glossary'
+]
+
 
 @blueprint.get('/')
-def list_creatures():
-    packs_dir = foundry.pf2e_pack_dir()/'packs'
-    packs = foundry.pf2e_pack_dir().glob('packs/*-bestiary')
-    creatures = (
-        (pack.name, (entry.relative_to(packs_dir)
-                     for entry in pack.glob('*.json')))
-        for pack in packs
-    )
-    return render_template('creature_list.j2.html', creatures=creatures)
+def list_packs():
+    return render_template('pack_list.j2.html',
+        packs=[pack.name
+               for pack in (foundry.pf2e_pack_dir()/'packs').iterdir()
+               if 'bestiary' in pack.name and pack.name not in _EXCLUDED_PACKS])
+
+
+@blueprint.get('/pack/<string:pack>')
+def list_creatures(pack: str):
+    path = foundry.pf2e_pack_dir()/'packs'/pack
+    return render_template('creature_list.j2.html', pack=pack,
+        creatures=[creature.stem for creature in path.glob('*.json')])
 
 
 @blueprint.get('/view/<path:id>')
