6398e166f650567ec9f6e9da3f1d862ac1dd1cf5
Daomephsta <dtnilfisk@gmail.com>
2024-04-22 22:09:04 +1200

    Add save/load feature to Initiative Tracker

--- encounter/ttrpg_scribe/encounter/flask/static/encounter_engine.js
+++ encounter/ttrpg_scribe/encounter/flask/static/encounter_engine.js
@@ -14,7 +14,7 @@ function getRound()
     return Number(sessionStorage.getItem('current_round') || 1)
 }
 
-function setRound(round) 
+function setRound(round)
 {
     sessionStorage.setItem('current_round', round)
     document.getElementById('current_round').textContent = `Round ${round}`
@@ -22,7 +22,7 @@ function setRound(round)
 
 function getInitiative(row)
 {
-    return row.querySelector('td:nth-child(2) > input').valueAsNumber
+    return row.querySelector('.initiative').valueAsNumber
 }
 
 function sortInitiative()
@@ -38,7 +38,7 @@ function nextTurn()
 {
     rows = document.querySelectorAll('#tracker tbody > tr')
     setCurrentTurn(currentTurn + 1)
-    if (currentTurn == 0) 
+    if (currentTurn == 0)
         setRound(getRound() + 1)
     // Skip dead creatures
     rows = document.querySelectorAll('#tracker tbody > tr')
@@ -67,6 +67,77 @@ function updateReinforcementControls()
         creature.classList.remove('collapsed')
         quick.classList.add('collapsed')
     }
+}
+
+function saveRow(row)
+{
+    const type = row.className
+    const name = row.querySelector('.name').textContent
+    const json = {
+        type: type,
+        initiative: getInitiative(row),
+        notes: row.querySelector('.notes').value,
+    }
+    console.log(json.name)
+    if (type != 'player') {
+        json.damage = row.querySelector('.damage').valueAsNumber
+    }
+    return [name, json]
+}
+
+function save()
+{
+    const json = {}
+    for (const row of document.querySelectorAll('#tracker tbody tr'))
+    {
+        let [name, rowJson] = saveRow(row)
+        json[name] = rowJson
+    }
+    const blob = new Blob([JSON.stringify(json)], {
+        type: 'application/json',
+    })
+    const url = URL.createObjectURL(blob)
+    const downloadLink = document.createElement('a')
+    downloadLink.href = url
+    downloadLink.download = `encounter_${new Date().toISOString()}.json`
+
+    document.body.appendChild(downloadLink)
+    downloadLink.click()
+    document.body.removeChild(downloadLink)
+    URL.revokeObjectURL(url)
+}
+
+function loadRow(row, json)
+{
+    row.classList.add(json.type)
+    row.querySelector('.initiative').value = json.initiative
+    row.querySelector('.notes').value = json.notes
+    if (json.type != 'player') {
+        row.querySelector('.damage').value = json.damage
+    }
+}
+
+function loadFrom(json) {
+    for (const row of document.querySelectorAll('#tracker tbody tr'))
+    {
+        const name = row.querySelector('.name').textContent
+        loadRow(row, json[name])
+    }
+    sortInitiative()
+}
+
+function load() {
+    const loadDialog = document.getElementById('load_dialog')
+    loadDialog.showModal()
+    const form = document.getElementById('load_dialog_form')
+    form.addEventListener('submit', (event) => {
+        files = event.target.file_picker.files
+        if (files.length > 0)
+            files[0].text().then(contents => loadFrom(JSON.parse(contents)))
+    },
+    {
+        once: true
+    })
 }
 
 onload = (_) =>  {
