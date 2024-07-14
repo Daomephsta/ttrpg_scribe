f6638141a68583a6492e0a9d1331bed3ce2d8f58
Daomephsta <dtnilfisk@gmail.com>
2024-07-15 10:13:26 +1200

    Clone a particular version of the Foundry PF2e system rather than the latest commit

--- pf2e_compendium/ttrpg_scribe/pf2e_compendium/foundry/packs.py
+++ pf2e_compendium/ttrpg_scribe/pf2e_compendium/foundry/packs.py
@@ -11,23 +11,27 @@ from ttrpg_scribe.pf2e_compendium.creature import (PF2Creature, Sense, Skill,
                                                    Spellcasting)
 from ttrpg_scribe.pf2e_compendium.foundry.enrich import enrich
 from ttrpg_scribe.pf2e_compendium.hazard import PF2Hazard
+import shutil
 
 type Json = dict[str, Any]
-_pf2e_pack = None
+VERSION = '6.0.4'
+_pf2e_dir = (platformdirs.user_data_path('ttrpg_scribe') / 
+                'pf2e_compendium/data/foundryvtt/pf2e').absolute()
 
 
 def pf2e_dir() -> Path:
-    global _pf2e_pack
-    if _pf2e_pack is not None:
-        return _pf2e_pack
-    _pf2e_pack = (platformdirs.user_data_path('ttrpg_scribe') /
-                  'pf2e_compendium/data/foundryvtt/pf2e').absolute()
-    if not _pf2e_pack.exists():
+    return _get_or_create_pf2e_dir()
+
+
+def _get_or_create_pf2e_dir():
+    if not _pf2e_dir.exists():
         subprocess.run([
-            'git', 'clone', '--depth', '1',
+            'git', 'clone',
+            '--depth', '1',
+            '--branch', VERSION,
             'https://github.com/foundryvtt/pf2e',
-            _pf2e_pack.as_posix()])
-    return _pf2e_pack
+            _pf2e_dir.as_posix()])
+    return _pf2e_dir
 
 
 def open_pf2e_file(path: str):
@@ -35,10 +39,15 @@ def open_pf2e_file(path: str):
 
 
 def update():
-    subprocess.run(
-        ['git', 'pull', '--depth', '1', '--rebase'],
-        cwd=pf2e_dir()
-    )
+    if _pf2e_dir.exists():
+        package_data = json.loads((_pf2e_dir/'package.json').read_text())
+        if package_data['version'] == VERSION:
+            print(f'PF2e system already compatible ({VERSION})')
+            return
+        else:
+            print(f'Replacing {package_data['version']} with {VERSION}')
+            shutil.rmtree(_pf2e_dir)
+    _get_or_create_pf2e_dir()
 
 
 def creature(id: str):
