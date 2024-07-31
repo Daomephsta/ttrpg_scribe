e9241db985ef98a6e53a04b3a21f28225c3d77d3
Daomephsta <dtnilfisk@gmail.com>
2024-07-31 18:13:05 +1200

    Improved statblock support for spontaneous spellcasting

--- pf2e_compendium/ttrpg_scribe/pf2e_compendium/creature/__init__.py
+++ pf2e_compendium/ttrpg_scribe/pf2e_compendium/creature/__init__.py
@@ -11,11 +11,17 @@ from ttrpg_scribe.pf2e_compendium.creature.statistics import (
 
 @dataclass
 class Spellcasting:
-    name: str
+    kind: str
     tradition: str
     dc: int
     attack: int
     spells: dict[int, list[str]] = field(default_factory=dict)
+    slots: dict[int, int] = field(default_factory=dict)
+    name: str = ''
+
+    def __post_init__(self):
+        if not self.name:
+            self.name = f'{self.kind} {self.tradition} spells'.title()
 
     def iter_spells(self):
         for level, spells in self.spells.items():
@@ -27,21 +33,25 @@ class Spellcasting:
 
     def to_json(self) -> dict[str, Any]:
         return dict(
-            name=self.name,
+            kind=self.kind,
             tradition=self.tradition,
             dc=self.dc,
             attack=self.attack,
-            spells=self.spells
+            spells=self.spells,
+            slots=self.slots,
+            name=self.name,
         )
 
     @staticmethod
     def from_json(data: dict):
         return Spellcasting(
-            name=data['name'],
+            kind=data['kind'],
             tradition=data['tradition'],
             dc=data['dc'],
             attack=data['attack'],
-            spells={int(level): spells for level, spells in data['spells'].items()}
+            spells={int(level): spells for level, spells in data['spells'].items()},
+            slots={int(level): slots for level, slots in data['slots'].items()},
+            name=data['name'],
         )
 
 
