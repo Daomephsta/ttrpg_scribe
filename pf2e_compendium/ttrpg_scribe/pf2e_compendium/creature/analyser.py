895567a52fcb49253ac60bbd91e9a71027071e18
Daomephsta <dtnilfisk@gmail.com>
2024-09-26 23:05:33 +1200

    Limited analysis of non-strike actions

--- pf2e_compendium/ttrpg_scribe/pf2e_compendium/creature/analyser.py
+++ pf2e_compendium/ttrpg_scribe/pf2e_compendium/creature/analyser.py
@@ -1,11 +1,12 @@
 import math
+import re
 from dataclasses import dataclass
 
-from ttrpg_scribe.pf2e_compendium.actions import Strike
+from ttrpg_scribe.pf2e_compendium.actions import SimpleAction, Strike
 from ttrpg_scribe.pf2e_compendium.creature import PF2Creature
 from ttrpg_scribe.pf2e_compendium.creature.statistics import (
     ARMOR_CLASS, ATTRIBUTE_MODIFIERS, HIT_POINTS, PERCEPTION, SAVING_THROWS,
-    SKILLS, STRIKE_ATTACK_BONUS, STRIKE_DAMAGE)
+    SKILLS, SPELL_DC, STRIKE_ATTACK_BONUS, STRIKE_DAMAGE)
 
 
 @dataclass
@@ -19,15 +20,22 @@ class Report:
     hp: str
 
     @dataclass
-    class Strike:
+    class Attack:
         name: str
         bonus: str
         damage: str
-    strikes: list[Strike]
+    attacks: list[Attack]
+
+    @dataclass
+    class Action:
+        name: str
+        dc: str
+        damage: str
+    active_abilities: list[Action]
 
 
 def analyse(creature: PF2Creature):
-    def analyse_strike(strike: Strike) -> Report.Strike:
+    def analyse_strike(strike: Strike) -> Report.Attack:
         bonus_bracket = STRIKE_ATTACK_BONUS.classify(creature.level, strike.bonus)
         if strike.damage:
             average_damage = sum(
@@ -35,8 +43,19 @@ def analyse(creature: PF2Creature):
                 for amount, _ in strike.damage
             )
             damage_bracket = STRIKE_DAMAGE.classify(creature.level, average_damage)
-            return Report.Strike(strike.name, bonus_bracket, damage_bracket)
-        return Report.Strike(strike.name, bonus_bracket, '')
+            return Report.Attack(strike.name, bonus_bracket, damage_bracket)
+        return Report.Attack(strike.name, bonus_bracket, '')
+
+    attacks = []
+    actions = []
+    for action in creature.actions:
+        match action:
+            case Strike():
+                attacks.append(analyse_strike(action))
+            case SimpleAction():
+                dc = re.search(r'DC (\d+)', action.desc)
+                dc = SPELL_DC.classify(creature.level, int(dc[1])) if dc else ''
+                actions.append(Report.Action(action.name, dc, ''))
 
     return Report(
         creature.name,
@@ -49,5 +68,6 @@ def analyse(creature: PF2Creature):
         {save: SAVING_THROWS.classify(creature.level, value)
          for save, value in creature.saves.items()},
         HIT_POINTS.classify(creature.level, creature.max_hp),
-        [analyse_strike(action) for action in creature.actions if isinstance(action, Strike)]
+        attacks,
+        actions
     )
