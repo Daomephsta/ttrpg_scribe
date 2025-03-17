aad981009b8e674eacb8a92a5706b8b53c3ac709
Daomephsta <dtnilfisk@gmail.com>
2025-03-17 18:32:30 +1300

    `index.yml` for overriding page index order

--- notes/ttrpg_scribe/notes/content_tree.py
+++ notes/ttrpg_scribe/notes/content_tree.py
@@ -3,6 +3,7 @@ from enum import Enum, auto
 from functools import reduce
 from pathlib import Path
 import re
+import sys
 
 from ttrpg_scribe.core import markdown
 from ttrpg_scribe.notes import paths
@@ -55,18 +56,56 @@ class Content:
 
 
 def walk(path: str) -> Content:
-    def walk_subtree(dir: Path, subtree: Content):
-        for path in sorted(dir.iterdir(), key=lambda path: path.name):
-            if path.name in ['assets', '__pycache__'] or path.suffix == '.py':
+    def transform_index(index: dict | list) -> dict:
+        match index:
+            case dict():
+                return {k: transform_index(v) for k, v in index.items()}
+            case list():
+                transformed = {}
+                for i, e in enumerate(index):
+                    match e:
+                        case dict():
+                            k, v = next(iter(e.items()))
+                            transformed[k] = i, transform_index(v)
+                        case _:
+                            transformed[e] = i
+                return transformed
+
+    def load_index(dir: Path) -> dict:
+        if (dir/'index.yml').exists():
+            with (dir/'index.yml').open() as file:
+                return transform_index(yaml.safe_load(file))
+        return {}
+
+    def walk_subtree(dir: Path, subtree: Content, index: dict | tuple[int, dict]):
+        if isinstance(index, tuple):
+            _, index = index
+
+        def by_index(path: Path):
+            match index.get(path.name):
+                case (int() as ordinal, dict()):
+                    ordinal = ordinal
+                case int() as ordinal:
+                    ordinal = ordinal
+                case None:
+                    ordinal = sys.maxsize
+                case unknown:
+                    raise ValueError(unknown)
+            return ordinal, path.name
+
+        for path in sorted(dir.iterdir(), key=by_index):
+            if path.name in ['assets', '__pycache__', 'index.yml'] or path.suffix == '.py':
                 continue
             child = subtree.add_child(path)
             if path.is_dir():
-                walk_subtree(path, child)
+                walk_subtree(path, child, index.get(path.name, {}))
         return subtree
+
     root_path = paths.CAMPAIGN.get(path)
-    root = walk_subtree(root_path, Content(paths.CAMPAIGN, root_path, ''))
+    root = walk_subtree(root_path, Content(paths.CAMPAIGN, root_path, ''), load_index(root_path))
     if paths.SETTING is not None:
         setting_path = paths.SETTING.get(path)
         root.children['setting'] = walk_subtree(
-            setting_path, Content(paths.SETTING, setting_path, 'setting'))
+            setting_path, Content(paths.SETTING, setting_path, 'setting'),
+            load_index(setting_path))
     return root
