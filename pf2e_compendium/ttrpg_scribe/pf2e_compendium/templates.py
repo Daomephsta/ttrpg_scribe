be8dec5df0a41f84fb0daada1ea3839e76389b6f
Daomephsta <dtnilfisk@gmail.com>
2025-04-04 18:06:23 +1300

    Support passing multiple ids to variants()

--- pf2e_compendium/ttrpg_scribe/pf2e_compendium/templates.py
+++ pf2e_compendium/ttrpg_scribe/pf2e_compendium/templates.py
@@ -13,16 +13,19 @@ class SupportsTemplates(Protocol):
 type Variant = tuple[str, list[Template]] | tuple[list[Template], str]
 
 
-def variants[T: SupportsTemplates](factory: Callable[[str], T], base_id: str,
-                                   *variants: Variant) -> dict[str, T]:
-    base_key = base_id.rsplit('/', maxsplit=1)[-1].upper().replace('-', '_')
+def variants[T: SupportsTemplates](factory: Callable[[str], T], base_ids: str | list[str],
+                                   *arguments: Variant) -> dict[str, T]:
+    if isinstance(base_ids, str):
+        base_ids = [base_ids]
 
     def make():
-        for variant in variants:
-            match variant:
-                case str() as prefix, list() as templates:
-                    yield prefix + base_key, factory(base_id).apply(*templates)
-                case list() as templates, str() as suffix:
-                    yield base_key + suffix, factory(base_id).apply(*templates)
+        for base_id in base_ids:
+            base_key = base_id.rsplit('/', maxsplit=1)[-1].upper().replace('-', '_')
+            for variant in arguments:
+                match variant:
+                    case str() as prefix, list() as templates:
+                        yield prefix + base_key, factory(base_id).apply(*templates)
+                    case list() as templates, str() as suffix:
+                        yield base_key + suffix, factory(base_id).apply(*templates)
 
     return dict(make())
