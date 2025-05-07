b13a95cae5e7bb80a019b5a4b87312534c72e736
Daomephsta <dtnilfisk@gmail.com>
2025-05-07 19:26:48 +1200

    Add support for all link elements to popup.js

--- core/ttrpg_scribe/core/flask/static/popup.js
+++ core/ttrpg_scribe/core/flask/static/popup.js
@@ -1,7 +1,7 @@
-// Opens <a> links using the popup target as popups
+// Opens <a>, <area>, or <form> links using the popup target as popups
 onload = () => 
 {
-    for (const link of document.querySelectorAll("a[target=popup]"))
+    for (const link of document.querySelectorAll(":is(a, area, form)[target=popup]"))
     {
         link.addEventListener('click', () => window.open(link.href,
             'popup', 'width=400,height=450'))
