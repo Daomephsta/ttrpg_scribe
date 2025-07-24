7a94f23c552990482c33041147d73c4f342c4a88
Daomephsta <dtnilfisk@gmail.com>
2025-07-25 04:05:26 +1200

    Enrich and correct placement of hazard description

--- pf2e_compendium/ttrpg_scribe/pf2e_compendium/foundry/packs.py
+++ pf2e_compendium/ttrpg_scribe/pf2e_compendium/foundry/packs.py
@@ -228,7 +228,7 @@ def _read_hazard(json: Json) -> PF2Hazard:
         routine=enrich(details.routine(json) or ''),  # routine can be null
         actions=actions,
         reset=enrich(details.reset(json)),
-        description=system.details.description(json)
+        description=enrich(system.details.description(json))
     )
 
 
