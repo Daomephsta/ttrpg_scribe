49fd6b3d5faf7b555266912670e170f3dfa3aafc
Daomephsta <dtnilfisk@gmail.com>
2025-08-22 18:46:40 +1200

    Add fixture to update PF2e data before pack tests start

--- pf2e_compendium/test/test_packs.py
+++ pf2e_compendium/test/test_packs.py
@@ -1,5 +1,13 @@
+import pytest
+from ttrpg_scribe.pf2e_compendium import foundry
 from ttrpg_scribe.pf2e_compendium.foundry import packs
 
 
+@pytest.fixture(scope='session')
+def setup_mongo():
+    foundry.check_for_updates()
+
+
+@pytest.mark.usefixtures('setup_mongo')
 def test_read_document(test_document):
     packs.read(test_document)
