57ead0a45973c16b743b0f1e289798c89bcc7579
Daomephsta <dtnilfisk@gmail.com>
2025-08-22 23:48:40 +1200

    Update to PF2e Foundry system 7.4.1

--- pf2e_compendium/test/test_packs.py
+++ pf2e_compendium/test/test_packs.py
@@ -9,6 +9,12 @@ def setup_mongo():
 
 
 _BUGS: dict[str, tuple[type[Exception], str]] = {
+    'battlecry-bestiary/arboreal-copse': (
+        AssertionError, r"Unconsumed arguments \['optionsarea-effect'\]"
+    ),
+    'lost-omens-bestiary/shining-kingdoms/lamp-blighter': (
+        KeyError, 'rituals'
+    )
 }
 
 
