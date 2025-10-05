829769dfe161d64860b5c2f3740ea4ba2150ab25
Daomephsta <dtnilfisk@gmail.com>
2025-10-05 18:47:48 +1300

    Only setup typescript watch compilation in dev env with debug mode on

--- core/ttrpg_scribe/core/typescript.py
+++ core/ttrpg_scribe/core/typescript.py
@@ -8,8 +8,12 @@ from typing import overload
 import flask
 from werkzeug.exceptions import NotFound
 
+_IS_DEV = 'site-packages' not in __file__
+
 
 def ensure_compiled(instance_dir: Path):
+    if not _IS_DEV:
+        return
     (logs_dir := instance_dir/'logs').mkdir(parents=True, exist_ok=True)
     (build_dir := instance_dir/'_build').mkdir(parents=True, exist_ok=True)
 
@@ -50,7 +54,7 @@ def extend(scaffold: flask.Blueprint | flask.Flask):
 
 
 def static_javascript_patch(scaffold: flask.Blueprint | flask.Flask):
-    if scaffold.static_folder is None:
+    if not _IS_DEV or scaffold.static_folder is None:
         return
 
     @scaffold.get('/static/<path:filename>.js')
@@ -58,8 +62,9 @@ def static_javascript_patch(scaffold: flask.Blueprint | flask.Flask):
         assert scaffold.static_folder is not None
         if not filename.endswith('.js'):
             filename = f'{filename}.js'
-        if flask.current_app.debug:
-            assert __file__.endswith('core/ttrpg_scribe/core/typescript.py')
+        reload = flask.current_app.debug and 'site-packages' not in __file__
+        if reload:
+            assert __file__.endswith('core/ttrpg_scribe/core/typescript.py'), __file__
             root_project = Path(__file__.removesuffix('core/ttrpg_scribe/core/typescript.py'))
             root_relative = Path(scaffold.static_folder).relative_to(root_project)
             try:
