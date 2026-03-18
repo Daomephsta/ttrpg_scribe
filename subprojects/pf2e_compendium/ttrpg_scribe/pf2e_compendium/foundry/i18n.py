6a60932e894d59218a3ba710d585a9382c71517e
Daomephsta <dtnilfisk@gmail.com>
2026-03-18 23:45:32 +1300

    Update i18n to new language file locations

--- subprojects/pf2e_compendium/ttrpg_scribe/pf2e_compendium/foundry/i18n.py
+++ subprojects/pf2e_compendium/ttrpg_scribe/pf2e_compendium/foundry/i18n.py
@@ -1,5 +1,5 @@
 import json
-from typing import Any
+from typing import Any, cast
 
 from ttrpg_scribe.pf2e_compendium import foundry
 
@@ -9,16 +9,19 @@ _translations = {}
 
 
 def _initialise():
-    with (foundry.pf2e_dir/'static/lang/en.json').open(encoding='utf8') as file:
-        def visit(obj: Json, path: list[str]):
-            for k, v in obj.items():
-                match v:
-                    case str():
-                        _translations['.'.join(path + [k])] = v
-                    case dict():
-                        visit(v, path + [k])
+    def visit(obj: Json, path: list[str]):
+        for k, v in obj.items():
+            match v:
+                case str():
+                    _translations['.'.join(path + [k])] = v
+                case dict():
+                    visit(v, path + [k])
 
-        visit(json.load(file), [])
+    for language in foundry.system['languages']:
+        if language['lang'] != 'en':
+            continue
+        with (foundry.pf2e_dir/cast(str, language['path'])).open(encoding='utf8') as file:
+            visit(json.load(file), [])
 
 
 def translate(key: str):
