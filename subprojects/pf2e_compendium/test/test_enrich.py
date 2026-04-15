6a76ec65fe377e7d622f0af2e61eddd610136bf3
Daomephsta <dtnilfisk@gmail.com>
2026-04-16 01:20:34 +1200

    Update enricher tests for display handling changes

--- subprojects/pf2e_compendium/test/test_enrich.py
+++ subprojects/pf2e_compendium/test/test_enrich.py
@@ -12,8 +12,8 @@ TEST_CONTEXT = {
 }
 
 
-@pytest.mark.parametrize(['text', 'result'], [
-    ('@Damage[1d6[fire]]{ouch!}', 'ouch!'),
+@pytest.mark.parametrize(['text', 'expected'], [
+    ('@Damage[1d6[fire]]{ouch!}', ([('1d6', 'fire')], 'ouch!')),
     ('@Damage[(1d6 + 3)[fire]]', [('1d6 + 3', 'fire')]),
     ('@Damage[5d6[acid],5d6[cold],5d6[fire]]',
      [('5d6', 'acid'), ('5d6', 'cold'), ('5d6', 'fire')]),
@@ -24,23 +24,29 @@ TEST_CONTEXT = {
     ('@Damage[2d6[fire]|options:area-damage]', [('2d6', 'fire')]),
     ('@Damage[(@actor.level)d6[fire]]', [(f'{TEST_CONTEXT['actor']['level']}d6', 'fire')])
 ])
-def test_damage_enricher(text: str, result: str | list[tuple[str, str]]):
+def test_damage_enricher(text: str,
+                         expected: tuple[list[tuple[str, str]], str] | list[tuple[str, str]]):
     enriched = enrich(text, TEST_CONTEXT)
-    match result:
-        case str():
-            assert enriched == result
-        case _:
-            soup = BeautifulSoup(enriched, features='html.parser')
-            assert len(soup) == 1, 'Expected 1 top level tag'
-            [top] = soup
-            assert isinstance(top, Tag), 'Expected top level tag'
+    soup = BeautifulSoup(enriched, features='html.parser')
+    assert len(soup) == 1, 'Expected 1 top level element'
+    [top] = soup
+    assert isinstance(top, Tag), 'Expected top element to be tag'
+    match expected:
+        case list() as damage, display:
+            [damage_tag, display_text] = top.children
+            assert isinstance(damage_tag, Tag), 'Expected first child to be tag'
+            assert isinstance(display_text, str), 'Expected second child to be str'
+            assert display_text == display
+        case list() as damage:
+            damage_tag = top
+            assert isinstance(damage_tag, Tag), 'Expected child to be tag'
 
-            def extract(t) -> tuple[str, str]:
-                amount, categories = t
-                assert isinstance(amount, Tag), 'Expected amount tag'
-                assert isinstance(categories, str), 'Expected categories str'
-                return amount.text, categories.strip(' +')
+    def extract(t) -> tuple[str, str]:
+        amount, categories = t
+        assert isinstance(amount, Tag), 'Expected amount tag'
+        assert isinstance(categories, str), 'Expected categories str'
+        return amount.text, categories.strip(' +')
 
-            batches = itertools.batched(top.children, n=2)
-            for actual, expected in zip(map(extract, batches), result):
-                assert actual == expected
+    batches = itertools.batched(damage_tag.children, n=2)
+    for actual, expected_damage in zip(map(extract, batches), damage):
+        assert actual == expected_damage
