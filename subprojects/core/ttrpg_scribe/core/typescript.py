30310afee85a1d7af205c15d432a4cc327c3c42b
Daomephsta <dtnilfisk@gmail.com>
2026-05-15 01:00:43 +1200

    Unhardcode subprojects dir

--- subprojects/core/ttrpg_scribe/core/typescript.py
+++ subprojects/core/ttrpg_scribe/core/typescript.py
@@ -22,8 +22,7 @@ def ensure_compiled(instance_dir: Path):
     if rollup is None:
         logging.warning('rollup not installed')
         return
-    subprojects = Path('/home/david/TTRPGs/ttrpg_scribe/subprojects')
-    for subproject in subprojects.iterdir():
+    for subproject in __subprojects().iterdir():
         if not (subproject/'rollup.config.mjs').exists():
             continue
         watch_log = (logs_dir/f'rollup_watch_{subproject.name}.log').open('w')
@@ -66,8 +65,7 @@ def static_javascript_patch(scaffold: flask.Blueprint | flask.Flask):
         reload = flask.current_app.debug and 'site-packages' not in __file__
         if reload:
             assert __file__.endswith('core/ttrpg_scribe/core/typescript.py'), __file__
-            root_project = Path(__file__.removesuffix('core/ttrpg_scribe/core/typescript.py'))
-            root_relative = Path(scaffold.static_folder).relative_to(root_project)
+            root_relative = Path(scaffold.static_folder).relative_to(__subprojects())
             try:
                 return flask.send_from_directory(
                     Path(flask.current_app.instance_path)/'_build'/root_relative,
@@ -77,3 +75,8 @@ def static_javascript_patch(scaffold: flask.Blueprint | flask.Flask):
                 return flask.send_from_directory(scaffold.static_folder, filename)
         else:
             return flask.send_from_directory(scaffold.static_folder, filename)
+
+
+def __subprojects():
+    assert __file__.endswith('core/ttrpg_scribe/core/typescript.py'), __file__
+    return Path(__file__.removesuffix('core/ttrpg_scribe/core/typescript.py'))
